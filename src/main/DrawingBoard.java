6e45e315c5ccb584ddf9e3454a71a3198a4d29b9
kornkritpawit <konsoo@hotmail.com>
2020-04-07 21:42:46 +0700

    finish

--- src/main/DrawingBoard.java
+++ src/main/DrawingBoard.java
@@ -32,10 +32,21 @@ public class DrawingBoard extends JPanel {
 	
 	public void groupAll() {
 		// TODO: Implement this method.
+
+		CompositeGObject bigObject = new CompositeGObject();
+		for(GObject go : gObjects){
+			bigObject.add(go);
+		}
+		gObjects.clear();
+		addGObject(bigObject);
+		bigObject.recalculateRegion();
+		repaint();
 	}
 
 	public void deleteSelected() {
 		// TODO: Implement this method.
+		gObjects.remove(target);
+		repaint();
 	}
 	
 	public void clear() {
@@ -78,19 +89,53 @@ public class DrawingBoard extends JPanel {
 	class MAdapter extends MouseAdapter {
 
 		// TODO: You need some variables here
-		
+		private int current_x;
+		private int current_y;
+		boolean drag;
 		private void deselectAll() {
 			// TODO: Implement this method.
+			for(GObject go: gObjects){
+				go.deselected();
+
+			}
+			drag = false;
 		}
 		
 		@Override
 		public void mousePressed(MouseEvent e) {
 			// TODO: Implement this method.
+			current_x = e.getX();
+			current_y = e.getY();
+			deselectAll();
+			System.out.println("press X: "+e.getX()+" press Y: "+e.getY());
+			for(GObject go: gObjects){
+				if(go.pointerHit(current_x,current_y)) {
+					go.selected();
+					target = go;
+					drag = true;
+					System.out.println(current_x);
+					System.out.println(current_y);
+				}
+
+			}
+			repaint();
+
 		}
 
 		@Override
 		public void mouseDragged(MouseEvent e) {
 			// TODO: Implement this method.
+			int desX = e.getX() - current_x ;
+			int desY = e.getY() - current_y ;
+			if (drag){
+				target.move(desX,desY);
+//				System.out.println("dx: "+ desX+ " dY: "+ desY);
+				current_x = e.getX();
+				current_y = e.getY();
+				repaint();
+			}
+
+
 		}
 	}
 	
--- src/objects/CompositeGObject.java
+++ src/objects/CompositeGObject.java
@@ -16,29 +16,70 @@ public class CompositeGObject extends GObject {
 
 	public void add(GObject gObject) {
 		// TODO: Implement this method.
+		gObjects.add(gObject);
 	}
 
 	public void remove(GObject gObject) {
 		// TODO: Implement this method.
+		gObjects.remove(gObject);
 	}
 
 	@Override
 	public void move(int dX, int dY) {
 		// TODO: Implement this method.
+		this.x += dX;
+		this.y += dY;
+		for(GObject go: gObjects){
+//			go.x +=dX;
+//			go.y +=dY;
+			go.move(dX,dY);
+		}
 	}
 	
 	public void recalculateRegion() {
 		// TODO: Implement this method.
+		GObject obj = gObjects.get(0);
+		int min_x = obj.x;
+		int min_y = obj.y;
+		int max_x = min_x+ obj.width;
+		int max_y = min_y+ obj.height;
+
+		for( GObject go: gObjects){
+			if(min_x> go.x){
+				min_x = go.x;
+			}
+			if(min_y>go.y){
+				min_y = go.y;
+			}
+			if(max_x < go.x + go.width){
+				max_x = go.x + go.width;
+			}
+			if(max_y < go.y+ go.height){
+				max_y = go.y + go.height;
+			}
+			this.x = min_x;
+			this.y = min_y;
+			this.width = max_x -min_x;
+			this.height = max_y - min_y;
+		}
+		System.out.println("x: "+min_x+" y: "+min_y);
+		System.out.println("width: "+width+" height: "+height);
 	}
 
 	@Override
 	public void paintObject(Graphics g) {
 		// TODO: Implement this method.
+		for(GObject go : gObjects){
+			go.paintObject(g);
+		}
+
 	}
 
 	@Override
 	public void paintLabel(Graphics g) {
 		// TODO: Implement this method.
+		g.drawString("Group",x+40, y+height+20);
+
 	}
 	
 }
