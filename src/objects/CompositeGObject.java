8c0d7c7b3c5c21ba6166394c869f12618e55d82d
Tiranan <tiranan.e@ku.th>
2020-04-08 22:53:59 +0700

    finish Composite

--- src/objects/CompositeGObject.java
+++ src/objects/CompositeGObject.java
@@ -16,29 +16,66 @@ public class CompositeGObject extends GObject {
 
 	public void add(GObject gObject) {
 		// TODO: Implement this method.
+		gObjects.add(gObject);
 	}
 
 	public void remove(GObject gObject) {
 		// TODO: Implement this method.
+		gObjects.remove(gObject);
 	}
 
 	@Override
 	public void move(int dX, int dY) {
 		// TODO: Implement this method.
+		this.x += dX;
+		this.y += dY;
+		for (GObject gObject : gObjects){
+			gObject.x += dX;
+			gObject.y += dY;
+			gObject.move(dX,dY);
+		}
+
 	}
 	
 	public void recalculateRegion() {
 		// TODO: Implement this method.
+		GObject gObject = gObjects.get(0);
+		int min_x = gObject.x;
+		int min_y = gObject.y;
+		int max_x = min_x+ gObject.width;
+		int max_y = min_y+ gObject.height;
+		for( GObject gObject1: gObjects){
+			if(min_x> gObject1.x){
+				min_x = gObject1.x;
+			}
+			if(max_x < gObject1.x + gObject1.width){
+				max_x = gObject1.x + gObject1.width;
+			}
+			if(min_y>gObject1.y){
+				min_y = gObject1.y;
+			}
+			if(max_y < gObject1.y+ gObject1.height){
+				max_y = gObject1.y + gObject1.height;
+			}
+			this.x = min_x;
+			this.y = min_y;
+			this.width = max_x - min_x;
+			this.height = max_y - min_y;
+		}
 	}
-
+	
 	@Override
 	public void paintObject(Graphics g) {
 		// TODO: Implement this method.
+		for(GObject gObject : gObjects){
+			gObject.paintObject(g);
+		}
 	}
 
 	@Override
 	public void paintLabel(Graphics g) {
 		// TODO: Implement this method.
+		g.drawString("Group",x,y+height+12);
 	}
 	
 }
